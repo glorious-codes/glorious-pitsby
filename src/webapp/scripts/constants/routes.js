973bf0cafc56b853f71b6f410ea5dcc825df905f
Rafael Camargo <contato@rafaelcamargo.com>
2018-10-28 17:39:48 -0300

    Create external components view

--- src/webapp/scripts/constants/routes.js
+++ src/webapp/scripts/constants/routes.js
@@ -1,5 +1,15 @@
 import home from '@scripts/views/home.html';
+import externalComponents from '@scripts/views/external-components.html';
 
 export default [
-  { name: 'home', url: '/', template: home }
+  {
+    name: 'home',
+    url: '/',
+    template: home
+  },
+  {
+    name: 'external-components',
+    url: '/components',
+    template: externalComponents
+  }
 ];
