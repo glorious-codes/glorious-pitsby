e0f63b27aba9eeb9fb9ff3792955506675d0736d
Rafael Camargo <hello@rafaelcamargo.com>
2018-11-17 20:14:48 -0200

    Improve external component example

--- src/webapp/scripts/components/external-component-example/external-component-example.js
+++ src/webapp/scripts/components/external-component-example/external-component-example.js
@@ -1,3 +1,4 @@
+import '@styles/external-component-example.styl';
 import template from './external-component-example.html';
 
 export default {
--- src/webapp/scripts/components/external-component-example/external-component-example.test.js
+++ src/webapp/scripts/components/external-component-example/external-component-example.test.js
@@ -9,7 +9,7 @@ describe('External Component Example', () => {
         const template = `<p-external-component-example
                             data-example="example">
                           </p-external-component-example>`;
-        scope.example = bindings.example;
+        scope.example = bindings.example || {};
         const element = $compile(template)(scope);
         scope.$digest();
         return element;
@@ -20,5 +20,16 @@ describe('External Component Example', () => {
   it('should have appropriate css class', () => {
     const element = compile();
     expect(element.find('div').attr('class')).toEqual('p-external-component-example');
+  });
+
+  it('should render a title if example title has been given', () => {
+    const title = 'Example Title';
+    const element = compile({example: {title}});
+    expect(element.find('h4').text()).toEqual(title);
+  });
+
+  it('should not render a title if no example title has been given', () => {
+    const element = compile();
+    expect(element.find('h4')[0]).toEqual(undefined);
   });
 });
