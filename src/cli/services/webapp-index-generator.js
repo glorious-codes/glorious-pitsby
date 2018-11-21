a0adef723ac8ee3991b263592411d2034a71cc9c
Rafael Camargo <hello@rafaelcamargo.com>
2018-11-20 22:31:15 -0200

    Improve webapp index generator

--- src/cli/services/webapp-index-generator.js
+++ src/cli/services/webapp-index-generator.js
@@ -3,7 +3,7 @@ const { fileService } = require('./file');
 
 const _public = {};
 
-_public.init = (clientDirectory, externalModuleName) => {
+_public.init = externalModuleName => {
   fileService.write(
     path.join(__dirname, '../../webapp/scripts/index.js'),
     buildIndex(externalModuleName)
