383b60598d7619fea0317737b963eea44e12d661
Rafael Camargo <hello@rafaelcamargo.com>
2018-11-20 22:06:41 -0200

    Set query param key on external component example tabs

--- src/webapp/scripts/components/external-component-example/external-component-example.js
+++ src/webapp/scripts/components/external-component-example/external-component-example.js
@@ -1,9 +1,27 @@
 import '@styles/external-component-example.styl';
 import template from './external-component-example.html';
 
+function controller(){
+  const $ctrl = this;
+
+  $ctrl.$onInit = () => {
+    setTabsQueryParamKey(buildTabsQueryParamKey($ctrl.exampleIndex));
+  };
+
+  function buildTabsQueryParamKey(exampleIndex){
+    return `externalComponentExample${exampleIndex}Tab`;
+  }
+
+  function setTabsQueryParamKey(key){
+    $ctrl.tabsQueryParamKey = key;
+  }
+}
+
 export default {
   bindings: {
-    example: '<'
+    example: '<',
+    exampleIndex: '<'
   },
+  controller,
   template
 };
