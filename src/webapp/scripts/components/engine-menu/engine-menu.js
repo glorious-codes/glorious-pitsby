d3ae89c38efdf709ecdce09bae6baae33583993b
Rafael Camargo <hello@rafaelcamargo.com>
2019-02-11 09:52:55 -0200

    Select first project found after fetch projects successfully

--- src/webapp/scripts/components/engine-menu/engine-menu.js
+++ src/webapp/scripts/components/engine-menu/engine-menu.js
@@ -2,7 +2,7 @@ import '@styles/engine-menu.styl';
 import projectsResource from '@scripts/resources/projects';
 import template from './engine-menu.html';
 
-function controller() {
+function controller(routeService) {
   const $ctrl = this;
 
   $ctrl.$onInit = () => {
@@ -10,15 +10,43 @@ function controller() {
   };
 
   function fetchProjects(){
-    projectsResource.get().then(setProjects, err => {
+    projectsResource.get().then(onFetchProjectsSuccess, err => {
       console.log('Failed to get projects', err);
     });
+  }
+
+  function onFetchProjectsSuccess(projects){
+    setProjects(projects);
+    configMenuVisibility(projects);
+    if(!isNoProjectSelected())
+      selectProject(projects[0]);
   }
 
   function setProjects(projects){
     $ctrl.projects = projects;
   }
+
+  function configMenuVisibility(projects){
+    const shouldShow = projects.length > 1;
+    setMenuVisibility(shouldShow);
+  }
+
+  function setMenuVisibility(shouldShow){
+    $ctrl.shouldShowMenu = shouldShow;
+  }
+
+  function isNoProjectSelected(){
+    return routeService.getParams('engine');
+  }
+
+  function selectProject(project){
+    routeService.go('app.external-components', {
+      engine: project.engine
+    });
+  }
 }
+
+controller.$inject = ['routeService'];
 
 export default {
   controller,
