4d3f62c17169f8c02ea4c9b48fc49ee20a95bd11
Rafael Camargo <hello@rafaelcamargo.com>
2019-02-13 10:33:06 -0200

    Improve args service to get specific argument value

--- src/cli/services/args.js
+++ src/cli/services/args.js
@@ -1,7 +1,24 @@
 const _public = {};
 
-_public.getCliArgs = () => {
-  return process.argv.slice(2);
+_public.getCliArgs = argName => {
+  const allArgs = process.argv.slice(2);
+  return argName ? findArg(argName, allArgs): allArgs;
 };
+
+function findArg(argName, allArgs){
+  for (var i = 0; i < allArgs.length; i++) {
+    const arg = buildArgObject(allArgs[i]);
+    if(arg.name == argName)
+      return arg.value;
+  }
+}
+
+function buildArgObject(arg){
+  const argKeyValuePair = arg.split('=');
+  return {
+    name: argKeyValuePair[0],
+    value: argKeyValuePair[1] || true
+  };
+}
 
 module.exports = _public;
