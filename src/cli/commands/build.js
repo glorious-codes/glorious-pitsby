dd4529192d9eb2786519a2d517b07d08920a1bef
Rafael Camargo <hello@rafaelcamargo.com>
2019-02-15 09:57:47 -0200

    Simplify build command

--- src/cli/commands/build.js
+++ src/cli/commands/build.js
@@ -7,21 +7,11 @@ _public.exec = options => {
   const option = getOption(options);
   if(docService.isHelpFlag(option))
     return docService.log('build');
-  if(isValidOption(option))
-    return buildService.init(options);
-  return docService.logUnknownOption('build', option);
+  return buildService.init(options);
 };
 
 function getOption(options){
   return options ? options[0] : null;
-}
-
-function isValidOption(option){
-  return !option || ['-w', '--watch'].includes(getOptionName(option));
-}
-
-function getOptionName(option){
-  return option.split('=')[0];
 }
 
 module.exports = _public;
