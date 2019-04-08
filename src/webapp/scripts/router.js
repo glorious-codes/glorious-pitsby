d0e158c288eb98a623a3b9517fce26be97e23043
Rafael Camargo <hello@rafaelcamargo.com>
2019-04-07 23:29:28 -0300

    Apply analytics service into the application

--- src/webapp/scripts/router.js
+++ src/webapp/scripts/router.js
@@ -1,12 +1,14 @@
+import analyticsService from '@scripts/services/analytics';
 import routes from '@scripts/constants/routes';
 
-function router($stateProvider, $urlRouterProvider) {
+function router($stateProvider, $urlRouterProvider, $transitionsProvider) {
   routes.forEach(route => {
     $stateProvider.state(route);
   });
   $urlRouterProvider.otherwise('/');
+  $transitionsProvider.onSuccess({}, analyticsService.trackPageView);
 }
 
-router.$inject = ['$stateProvider', '$urlRouterProvider'];
+router.$inject = ['$stateProvider', '$urlRouterProvider', '$transitionsProvider'];
 
 export default router;
