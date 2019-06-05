4cf18b80b7606ec7de20e18d17e0baf66f2cef33
Rafael Camargo <hello@rafaelcamargo.com>
2019-06-04 21:41:32 -0300

    Add logo generation on CI preparation steps

--- .circleci/prepare.js
+++ .circleci/prepare.js
@@ -3,6 +3,7 @@ const path = require('path');
 const externalMetricsIdsModuleGenerator = require('../src/cli/services/external-metrics-ids-module-generator');
 const webappHtmlIndexGenerator = require('../src/cli/services/webapp-html-index-generator');
 const webappIndexGenerator = require('../src/cli/services/webapp-index-generator');
+const webappLogoGenerator = require('../src/cli/services/webapp-logo-generator');
 
 function createDirectory(directoryPath){
   fs.mkdirSync(path.join(__dirname, directoryPath));
@@ -14,3 +15,4 @@ createDirectory('../src/webapp/external/');
 externalMetricsIdsModuleGenerator.init();
 webappHtmlIndexGenerator.init();
 webappIndexGenerator.init();
+webappLogoGenerator.init({});
