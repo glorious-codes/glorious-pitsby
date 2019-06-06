aea99908b9a1be8e7ff763fef7a9681d232e789e
Rafael Camargo <hello@rafaelcamargo.com>
2019-06-06 14:08:51 -0300

    Add ability to use a favicon from external url

--- src/cli/services/webapp-html-index-customisation.js
+++ src/cli/services/webapp-html-index-customisation.js
@@ -12,8 +12,12 @@ function handleFavicon({ filepath } = {}, template){
 }
 
 function buildFaviconHref(filepath){
-  const href = filepath ? path.join('external', filepath) : 'images/favicon-pitsby.png';
+  const href = filepath ? buildCustomFaviconHref(filepath) : 'images/favicon-pitsby.png';
   return `${href}?t=${Date.now()}`;
+}
+
+function buildCustomFaviconHref(filepath){
+  return filepath.includes('http') ? filepath :path.join('external', filepath);
 }
 
 function handleWindowTitle(title = 'Pitsby', template){
