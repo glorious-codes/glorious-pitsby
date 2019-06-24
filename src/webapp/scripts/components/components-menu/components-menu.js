baab1e4665fa022fdc94d1caf384b630d929237d
Rafael Camargo <hello@rafaelcamargo.com>
2019-06-23 21:09:06 -0300

    Crete components menu component

---
+++ src/webapp/scripts/components/components-menu/components-menu.js
@@ -0,0 +1,55 @@
+import '@styles/components-menu.styl';
+import componentsMenuService from '@scripts/services/components-menu';
+import template from './components-menu.html';
+
+function controller($transitions, routeService){
+  const $ctrl = this;
+
+  $ctrl.$onInit = () => {
+    setTransitions($transitions);
+    listenRouteChange();
+  };
+
+  $ctrl.fetch = () => {
+    routeService.getAllRoutes();
+    return componentsMenuService.build(routeService.getParams('engine'));
+  };
+
+  $ctrl.fetchSuccess = items => {
+    setItems(items);
+    configActiveItem(items);
+  };
+
+  function setTransitions(transitions){
+    $ctrl.$transitions = transitions;
+  }
+
+  function listenRouteChange(){
+    $ctrl.$transitions.onSuccess({}, () => {
+      configActiveItem($ctrl.items);
+    });
+  }
+
+  function setItems(items){
+    $ctrl.items = items;
+  }
+
+  function configActiveItem(items = []){
+    return items.forEach(item => {
+      if(item.children)
+        return configActiveItem(item.children);
+      item.active = isCurrentRoute(item.route);
+    });
+  }
+
+  function isCurrentRoute(route){
+    return routeService.isCurrentRoute(route.name, route.params);
+  }
+}
+
+controller.$inject = ['$transitions', 'routeService'];
+
+export default {
+  controller,
+  template
+};
