b1869761b9f4c4f637bf0beb3721d1a3903ff38d
Rafael Camargo <hello@rafaelcamargo.com>
2019-10-17 20:13:37 -0300

    chore: improve vue component builder
    Allow self closing tags on vue templates

--- src/webapp/scripts/services/vue-component-builder.test.js
+++ src/webapp/scripts/services/vue-component-builder.test.js
@@ -17,7 +17,7 @@ describe('Vue Component Builder', () => {
       template: '<p>Hello</p>'
     };
     const vm = vueComponentBuilder.build(component, container);
-    expect(vm.$el.querySelector('p').innerHTML).toEqual('Hello');
+    expect(vm.$el.innerHTML).toEqual('Hello');
   });
 
   it('should render a component containing a controller', () => {
@@ -39,20 +39,19 @@ describe('Vue Component Builder', () => {
       template: '<button @click="greet">Greet</button>'
     };
     const vm = vueComponentBuilder.build(component, container);
-    vm.$el.querySelector('button').click();
+    vm.$el.click();
     expect(window.alert).toHaveBeenCalledWith('Rafael');
   });
 
   it('should append component into the given container', () => {
-    const container = createContainer();
-    const component = { template: '<p></p>' };
-    vueComponentBuilder.build(component, container);
-    expect(container.querySelectorAll('p').length).toEqual(1);
+    const component = { template: '<div><p></p></div>' };
+    const vm = vueComponentBuilder.build(component, createContainer());
+    expect(vm.$el.querySelectorAll('p').length).toEqual(1);
   });
 
   it('should do nothing if no component has been given', () => {
     const container = createContainer();
     vueComponentBuilder.build(undefined, container);
-    expect(container.innerText).toEqual(undefined);
+    expect(container.innerHTML).toEqual('');
   });
 });
