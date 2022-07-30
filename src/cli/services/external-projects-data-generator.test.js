b12997ba344893b5532f9b610d09ac351068b57f
Rafael Camargo <hello@rafaelcamargo.com>
2022-07-30 18:26:28 -0300

    chore: improve external projects data generator test suite

--- src/cli/services/external-projects-data-generator.test.js
+++ src/cli/services/external-projects-data-generator.test.js
@@ -3,27 +3,28 @@ const externalProjectsDataGenerator = require('./external-projects-data-generato
 
 describe('External Projects Data Generator', () => {
   beforeEach(() => {
-    webappDataService.save = jest.fn();
+    webappDataService.save = jest.fn((filename, data, onSuccess, onError) => onSuccess());
   });
 
-  it('should return a promise on save', () => {
-    const promise = externalProjectsDataGenerator.init();
-    expect(promise.then).toBeDefined();
-  });
-
-  it('should save data containing projects engines', () => {
+  it('should save data containing projects engines', done => {
     externalProjectsDataGenerator.init([
-      {engine: 'angular', moduleName: 'ng-components'},
-      {engine: 'vue', importFrom: './dist/vue-components'}
-    ]);
-    expect(webappDataService.save.mock.calls[0][0]).toEqual('projects.json');
-    expect(webappDataService.save.mock.calls[0][1]).toEqual([ {engine: 'angular'}, {engine: 'vue'} ]);
-    expect(typeof webappDataService.save.mock.calls[0][2]).toEqual('function');
-    expect(typeof webappDataService.save.mock.calls[0][3]).toEqual('function');
+      { engine: 'angular', moduleName: 'ng-components' },
+      { engine: 'vue', importFrom: './dist/vue-components' }
+    ]).then(() => {
+      expect(webappDataService.save).toHaveBeenCalledWith(
+        'projects.json',
+        [{ engine: 'angular' }, { engine: 'vue' }],
+        expect.any(Function),
+        expect.any(Function)
+      )
+      done();
+    });
   });
 
-  it('should save empty array data if no projects have been given', () => {
-    externalProjectsDataGenerator.init();
-    expect(webappDataService.save.mock.calls[0][1]).toEqual([]);
+  it('should save empty array data if no projects have been given', done => {
+    externalProjectsDataGenerator.init().then(() => {
+      expect(webappDataService.save.mock.calls[0][1]).toEqual([]);
+      done();
+    });
   });
 });
